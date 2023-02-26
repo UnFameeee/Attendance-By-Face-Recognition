7710e29abefcc01e1ab81eb88af1f06a23f4e814
UnFameeee <quocthang036@gmail.com>
2023-02-26 19:20:06 +0700

    Finish Setup Deploy version
    Finish Setup Deploy version - with Docker-compose (2/26/2023)

--- Source/backend/prisma/migrations/20230213104812_first_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE `Salary` (
-    `id` VARCHAR(191) NOT NULL,
-    `amount` INTEGER NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
