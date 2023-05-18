7d52cae3bf014e4478e976fd3f9196cc11978f9c
Quốc Thắng <quocthang036@gmail.com>
2023-05-19 02:15:37 +0700

    Update AttendancePersonal.js

--- Source/frontend/src/pages/home/Attendance/AttendancePersonal.js
+++ Source/frontend/src/pages/home/Attendance/AttendancePersonal.js
@@ -543,7 +543,7 @@ function AttendancePersonal() {
                       <VStack alignItems="start">
                         <Text fontSize="xl">Check in</Text>
                         <Text fontSize="2xl" fontWeight="bold">
-                          {moment(new Date(new Date(item?.checkIn).toISOString())).format("hh:mm") !=
+                          {moment(item?.checkIn).format("hh:mm") !=
                           "Invalid date"
                             ? moment(item?.checkIn).format("hh:mm")
                             : "--:--"}
