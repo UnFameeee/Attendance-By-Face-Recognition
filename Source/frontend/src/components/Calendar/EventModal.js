eec0d1bc4d2cb9f14865135f7ee803784669ffc3
DuongDuy <59405786+Duyduong689@users.noreply.github.com>
2023-05-31 09:43:10 +0700

    Update EventModal.js

--- Source/frontend/src/components/Calendar/EventModal.js
+++ Source/frontend/src/components/Calendar/EventModal.js
@@ -123,13 +123,13 @@ export default function EventModal(props) {
       if (selectedEvent.absent) {
         return true;
       }
-      let currentDate = new Date().toISOString().split('T')[0];
-      const formattedDate = moment(
-        selectedEvent?.shiftDate
-      ).format("YYYY-MM-DD")
+      let currentDate = new Date().toISOString().split("T")[0];
+      const formattedDate = moment(selectedEvent?.shiftDate).format(
+        "YYYY-MM-DD"
+      );
       return currentDate > formattedDate;
     } else {
-      let currentDate = new Date().toISOString().split('T')[0];
+      let currentDate = new Date().toISOString().split("T")[0];
       return currentDate > daySelected.format("YYYY-MM-DD");
     }
   }
@@ -145,8 +145,15 @@ export default function EventModal(props) {
             shiftDate: Helper.getMomentDateFormat(daySelected.valueOf()),
             shiftTypeId: values.shiftTypeId,
             employeeId: values.employeeId,
-            allowEarlyLeave: values.allowEarlyLeave == "true" ? true : false,
-            allowLateArrival: values.allowLateArrival == "true" ? true : false,
+            allowEarlyLeave:
+              values.allowEarlyLeave == "true" || values.allowEarlyLeave == true
+                ? true
+                : false,
+            allowLateArrival:
+              values.allowLateArrival == "true" ||
+              values.allowLateArrival == true
+                ? true
+                : false,
           };
           if (selectedEvent) {
             eventObj["shiftId"] = selectedEvent?.shiftId;
